3967754bf41ee60b069eb7732476ec9a93769e37
ameyagidh <ameyagb2@gmail.com>
2023-10-29 17:28:26 -0400

    done

---
+++ springbootkafka/src/test/java/com/wikistream/springbootkafka/SpringbootkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wikistream.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
